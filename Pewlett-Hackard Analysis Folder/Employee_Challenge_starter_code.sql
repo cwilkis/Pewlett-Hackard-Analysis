7869166463ef13a412a1bb3706b2fe847ebc532d
claudia wilkis <claudiajwilkis@gmail.com>
2022-04-24 16:49:21 -0500

    Analysis README file and updated code for challenge

--- Pewlett-Hackard Analysis Folder/Employee_Challenge_starter_code.sql
+++ Pewlett-Hackard Analysis Folder/Employee_Challenge_starter_code.sql
@@ -1,10 +1,74 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-----------------------------------------------------------------------------------------
+--Deliverable 1
+-----------------------------------------------------------------------------------------
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+--The Number of Retiring Employees by Title.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+LEFT JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY count DESC;
+
+-----------------------------------------------------------------------------------------
+--Deliverable 2
+-----------------------------------------------------------------------------------------
+
+--Mentorship Elegibility 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+LEFT JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+-----------------------------------------------------------------------------------------
+--Deliverable 3
+-----------------------------------------------------------------------------------------
+
+SELECT ut.emp_no,
+    ut.first_name,
+	ut.last_name,
+	ut.title,
+    e.birth_date,
+    de.to_date
+INTO oldest_retiring
+FROM unique_titles as ut
+INNER JOIN employees as e
+ON (ut.emp_no = e.emp_no)
+INNER JOIN dept_emp as de
+ON (ut.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1952-12-31')
+	 AND (de.to_date = '9999-01-01');
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Pewlett-Hackard-Analysis
+
+##Overview
+
+The purpose of this analysis was to determine the number of employees eligible for retirement and for a mentorship program. The request was to sort these deliverables by their employee number and filter by birtdate in order to determine eligibility.
+
+##Results
+
+Based on the retiring titles csv, over 70% of the eligible retirees, or 50,842 employees, are Senior Staff or Senior Engineers. 
+	- 25,916 Senior Engineers
+	- 24,926 Senior Staff
+
+This totals 17% of all employees at Pewlett-Hackard. 
+
+An additional 738 Senior Staff and Senior Engineers are elible for the mentorship program, or 48% of the total eligible pool. 
+
+##Summary
+
+The US Bureau Labor Statistics tracks turnover rates by industry. Professional and business services hovers between a 5.1-5.6% turnover rate (https://www.bls.gov/news.release/jolts.t03.htm). Pewlett-Hackard is looking at a 17% turnover rate if all eligible employees retire at the end of the year. That does not include any employees that would leave voluntarily for other opportunities. Due to this large scale retirement, my recommendation is that Packard-Hewlett offer voluntary retirement packages based on years of service, in order to space out the amount of employees retiring at once. Packard-Hewlett should also look at promoting staff and engineers to senior positions, and fill the vacant staff and engineer positions. This would allow a wider pool of candidates as the requirements are not as strenuous as Senior level positions.
+
